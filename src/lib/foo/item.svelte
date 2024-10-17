47c1d2849dc5542552a197152ef7e3ac5ae0374e
alex-pirogov <pirogovalexandr00@mail.ru>
2024-10-17 15:03:51 +0300

    initial commit

---
+++ src/lib/foo/item.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+	import { fade } from 'svelte/transition'
+
+	let { children }: {children: Snippet} = $props()
+</script>
+
+<div in:fade={{ duration: 150 }}>{@render children()}</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import * as Foo from "$lib/foo";
+    import Item from "$lib/foo/item.svelte";
+
+    let error = $state(undefined);
+</script>
+
+<input
+    oninput={(e) => {
+        error = e.target.value === "e" ? "my error" : undefined;
+    }}
+/>
+
+{#if error}
+    <Foo.Item>Error</Foo.Item>
+    <Item>Error</Item>
+{:else}
+    <Foo.Item>Message</Foo.Item>
+    <Item>Message</Item>
+{/if}
